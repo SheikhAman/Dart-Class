23faa577e5b22fcd2989ebde76dc2e79b1dd8d7b
ShekAdnan <adnansk418@gmail.com>
2021-11-25 19:45:03 +0600

    output nul and nul

---
+++ myapp/lib/main.dart
@@ -0,0 +1,23 @@
+void main() {
+  var student1 =
+      Student(); // kono variable r vitore class k contain kora e holo object create kora, amra student1 object create korechi
+  print('${student1.id} and ${student1.name}');
+}
+
+// Define states (properties  example : id and name) and (behavior example:studey and sleep method) of a Student
+class Student {
+  late int id; // we have defined some properties
+  late String name; // we have defined some properties
+
+  void study() {
+    // And alos defined some behavior
+
+    // Your code
+  }
+
+  void sleep() {
+    // And alos defined some behavior
+
+    // Your code
+  }
+}

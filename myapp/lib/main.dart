06ad6b26360fe3a8c0442febc75643e1fc8e9021
ShekAdnan <adnansk418@gmail.com>
2021-11-25 19:50:12 +0600

    instance variable or field variable

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -6,8 +6,10 @@ void main() {
 
 // Define states (properties  example : id and name) and (behavior example:studey and sleep method) of a Student
 class Student {
-  late int id; // we have defined some properties
-  late String name; // we have defined some properties
+  late int
+      id; // we have defined some properties // we have declared variable , this properties are known as the instance variable or the field variable
+  late String
+      name; // we have defined some properties  //  Instance or Field Variable // By default this property have default value of null
 
   void study() {
     // And alos defined some behavior
@@ -18,6 +20,6 @@ class Student {
   void sleep() {
     // And alos defined some behavior
 
-    // Your code
+    // Your codes
   }
 }
